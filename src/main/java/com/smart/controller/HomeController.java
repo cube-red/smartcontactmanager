cdd3f6271b5d5a13e3efd6c674dff91fce0f2b17
Aditya Lalbondre <bondreaditya99@gmail.com>
2023-06-18 01:15:22 +0530

    added controllers and html paages

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.dao.UserRepository;
@@ -10,16 +12,16 @@ import com.smart.entities.User;
 
 @Controller
 public class HomeController {
-	@Autowired
-	private UserRepository userRepository;
 	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		User user = new User();
-		user.setName("Aditya L");
-		user.setEmail("adityal@mail.com");
-		userRepository.save(user);
-		return "working...";
+	@RequestMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title","Home - Smart Contact Manager");
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title","About - Smart Contact Manager");
+		return "about";
 	}
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,5 @@
+/**
+ * 
+ */
+
+console.log("THis is Javascript");
