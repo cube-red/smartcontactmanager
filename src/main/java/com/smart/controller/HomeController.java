a9dec444446d17f02788cfd5802f8a43e9c8f32b
Aditya Lalbondre <bondreaditya99@gmail.com>
2023-06-18 00:27:27 +0530

    added tables User and Contact

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+
+@Controller
+public class HomeController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String test() {
+		User user = new User();
+		user.setName("Aditya L");
+		user.setEmail("adityal@mail.com");
+		userRepository.save(user);
+		return "working...";
+	}
+}
